334b1a540bd309f8de6620f3b3c0a5e8f48a6856
David Kakis <kakis@reinto.cz>
2019-03-17 15:25:12 +0100

    source code integration

---
+++ AcronymFromPhrase.Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AcronymFromPhrase.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("")]
+        public void When_EmptyString_Expect_EmptyString(string phrase)
+        {
+            Assert.That(AcronymFromPhrase.GenerateAcronym(phrase), Is.EqualTo(""));
+        }
+
+        [TestCase("Don't repeat yourself")]
+        public void When_PhraseDontRepeatYourSelf_Expect_DRY(string phrase)
+        {
+            Assert.That(AcronymFromPhrase.GenerateAcronym(phrase), Is.EqualTo("DRY"));
+        }
+        
+        [TestCase("Keep it simple stupid")]
+        public void When_PhraseKeepItSimpleStupid_Expect_KISS(string phrase)
+        {
+            Assert.That(AcronymFromPhrase.GenerateAcronym(phrase), Is.EqualTo("KISS"));
+        }
+        
+        [TestCase("Asynchronous Javascript and XML")]
+        public void When_PhraseAsynchronousJavascriptAndXml_Expect_AJAX(string phrase)
+        {
+            Assert.That(AcronymFromPhrase.GenerateAcronym(phrase), Is.EqualTo("AJAX"));
+        }
+        
+        [TestCase("Complementary metal-oxide semiconductor")]
+        public void When_PhraseComplementaryMetalOxideSemiconductor_Expect_CMOS(string phrase)
+        {
+            Assert.That(AcronymFromPhrase.GenerateAcronym(phrase), Is.EqualTo("CMOS"));
+        }
+        
+        
+        
+        
+    }
+}
---
+++ AcronymFromPhrase/AcronymFromPhrase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Char;
+
+namespace AcronymFromPhrase
+{
+  public static class AcronymFromPhrase
+  {
+//      IEnumerable<string>
+
+      private static string RemoveDashes(string phrase)
+      {
+          return phrase.Replace('-', ' ');
+      }
+      private static IEnumerable<string> ParsePhraseIntoWords (string phrase)
+      {
+          return  Regex.Replace(phrase, "[^a-zA-Z0-9% ._]", string.Empty).Split(' ');
+      
+      }
+      private static string ProcessWords(IEnumerable<string> words)
+      {
+          return words.Where(word => !string.IsNullOrEmpty(word)).Aggregate("", (current, word) => current + ToUpper(word[0]));
+      }
+      public static string GenerateAcronym(string phrase)
+      {
+     
+        return ProcessWords(ParsePhraseIntoWords(RemoveDashes(phrase)));
+
+      }
+      
+  }
+}
